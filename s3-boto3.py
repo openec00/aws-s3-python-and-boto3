8f44413d41c5cf068d84a1822ae17e20b5409d06
Chukwuma <33032472+openec00@users.noreply.github.com>
2021-08-24 14:32:53 +0100

    Updating project files

--- s3-boto3.py
+++ s3-boto3.py
@@ -13,8 +13,8 @@ from botocore.exceptions import ClientError
 
 
 #Create S3 bucket - in a production systems you should avoid hard coding your AWS access and secret keys
-AWS_ACCESS_KEY_ID = 'AKIAWS3UO4HZZVEZQ63U'
-AWS_SECRET_ACCESS_KEY = '<AWS_SECRET_ACCESS_KEY>'
+AWS_ACCESS_KEY_ID = '<ACCESS_KEY>'
+AWS_SECRET_ACCESS_KEY = '<SECRET_KEY>'
 
 #Create a s3 client object
 s3_client = boto3.client('s3', region_name ='eu-west-2',
@@ -84,6 +84,110 @@ s3_client.download_file(bucket_name, 'Downloads/awesomecsv.clone.csv', 'Target/a
 # Download and rename downloaded file using file handler (obj)
 with open('Target/awesomecsv.copy.csv', 'wb') as obj:
     s3_client.download_fileobj(bucket_name, myfile, obj)
+
+
+# In[15]:
+
+
+# Multipart file transfers
+# Multipart transfers is used when the file size exceeds the multipart_threshold attribute value.
+
+from boto3.s3.transfer import TransferConfig
+
+GByte = 1024**3
+
+config = TransferConfig(multipart_threshold=5*GByte)
+
+s3_client.upload_file(myfile, bucket_name, 'Downloads/awesomecsv.multipart.csv', Config=config)
+
+
+# In[17]:
+
+
+# Presigned URLs
+#allow a user without AWS credentials or permissons to access an S3 object
+# Presigned URLs grant temporary access for a limited period which is specified when the URL is generated
+#the presigned URL can be enttered into a browser
+
+url_response = s3_client.generate_presigned_url('get_object',
+                                                Params={'Bucket': bucket_name,
+                                                       'Key':myfile},
+                                                ExpiresIn= 3600) #3600seconds
+
+print(url_response)
+
+
+# In[21]:
+
+
+# Configuring Bucket polices
+# Bucket policies are defined in JSON.
+
+# Define a Bucket Policy
+import json
+
+bucket_name = 'my-python-boto3-project'
+bucket_policy = {
+    'Version':'2012-10-17',
+    'Statement':[{
+        'Sid':'AddPolicyPermission',
+        'Effect':'Allow',
+        'Principal':'*',
+        'Action':['s3:GetObject'],
+        'Resource':f'arn:aws:s3:::{bucket_name}/*'
+    }]
+}
+
+bucket_policy = json.dumps(bucket_policy)
+print(bucket_policy)
+s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy)
+
+
+# In[23]:
+
+
+# Bucket policy - Retrieve Bucket policy
+result = s3_client.get_bucket_policy(Bucket='my-python-boto3-project')
+print(result)
+
+
+# In[25]:
+
+
+# Bucket Policy - Delete Bucket policy
+s3_client.delete_bucket_policy(Bucket='my-python-boto3-project')
+
+
+# In[26]:
+
+
+# Setting Up a Bucket CORS Configuration 
+# CORS - Cross Origin Resource Sharing enables clients from one domain access resources in another domain.
+
+# You can use this method to allow sites access objects in your bucket.
+
+# Set Bucket CORS
+cors_configuration = {
+    'CORSRules': [
+        {'AllowedHeaders':['AUthorization'],
+        'AllowedMethods':['GET','PUT'],
+        'AllowedOrigins':["*"],
+        'ExposeHeaders':['GET','PUT'],
+        'MaxAgeSeconds':3000
+        }
+    ]
+}
+
+s3_client.put_bucket_cors(Bucket=bucket_name, CORSConfiguration = cors_configuration
+                         )
+
+
+# In[27]:
+
+
+# Retrieving a Bucket CORS Configuration
+cors_response = s3_client.get_bucket_cors(Bucket='my-python-boto3-project')
+print(cors_response['CORSRules'])
 
 
 # In[ ]:
